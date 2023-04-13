f6c3fa2a2ae691560ce699eb3d60f2303ae2ff8b
kiki <demarchi.christiann@gmail.com>
2023-04-13 11:06:03 -0300

    Sprint 1 SemiFinalizado

--- handsonlab/hol_02.js
+++ handsonlab/hol_02.js
@@ -10,7 +10,7 @@ class ProductManager {
     return this.products
   }
 
-  addProduct({ title, description, price, thumbnail, stock }) {
+  addProduct({ title, description, price, thumbnail, stock, code }) {
 
     let id = 1
 
@@ -21,22 +21,30 @@ class ProductManager {
       id = lastProduct.id + 1
     }
 
+    let product = { title, description, price, thumbnail, stock, code, id }
 
-
-    let product = { title, description, price, thumbnail, stock, id }
+    // for ( product in this.products) {
+    //   if (product.code === code){
+    //     console.error('Ya existe un producto con el mismo codigo')
+    //   } else {
+    //     this.products.push(product)
+    //   }
+    // }
 
     this.products.push(product)
+
   }
 
-  getProductById(id) {
+  getProductById(product_id) {
     
-    const product = this.products.find(p => p.id === parseInt(id))
+    const product = this.products.find(p => p.id === parseInt(product_id))
 
     if (product) {
       console.log(product)
       return product
     } else {
       console.error('Not found')
+      return null
     }
   }
 
@@ -44,4 +52,8 @@ class ProductManager {
 
 let product = new ProductManager()
 
-
+product.addProduct({ title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail:'Sin imagen', code: 'abc123', stock: 25})
+product.getProducts()
+product.addProduct({ title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail:'Sin imagen', code: 'abc123', stock: 25})
+product.getProductById(1)
+product.getProductById(3)
